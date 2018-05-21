db74f5d7dd3ff7b404fa2b7e3d85b70dac830550
Othryades <i.moris@yahoo.com>
2018-05-21 16:14:15 +0300

    add post todos route

---
+++ server/models/user.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+module.exports = {User};
+
+
+// var newUser = new User({
+//   email: 'zooby@zooby.com'
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log('Saved todo', doc);
+// }, (e) => {
+//   console.log('Unable to save todo')
+// });
